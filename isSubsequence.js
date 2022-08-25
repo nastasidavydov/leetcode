5cb897e527f7b218a1678281ef034a6ea1a7b2a2
Nastasi Davydov <nastasidavydov@gmail.com>
2022-08-24 22:41:43 -0400

    two pointers - check if a str is a subsequence of anotehr str

---
+++ isSubsequence.js
@@ -0,0 +1,21 @@
+/* Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not). */
+
+const isSubsequence = (s, t) => {
+
+  let subsequence = '';
+
+  let i = 0, j = 0;
+
+  while (i < t.length) {
+
+    if (t[i] === s[j]) {
+      subsequence += t[i];
+      j++;
+    }
+    i++;
+  }
+
+  return s === subsequence;
+}

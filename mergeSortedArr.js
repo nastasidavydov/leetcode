8785818c74ff617f2c456322013c00d5b721e521
Nastasi Davydov <nastasidavydov@gmail.com>
2022-09-06 21:01:12 -0400

    merge 2 sorted arr - 2 pointers

---
+++ mergeSortedArr.js
@@ -0,0 +1,34 @@
+/* You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n. */
+
+
+var merge = function(nums1, m, nums2, n) {
+    
+   
+  if (!n) return;
+  
+  let j = n - 1; //num2
+  let i = m - 1 //num1
+  
+  pos = m + n -1
+  
+  while (j >=0) {
+     
+      if (nums1[i] > nums2[j]) {
+          nums1[pos] = nums1[i];
+          i--;
+          
+      } else {
+
+          nums1[pos] = nums2[j];
+          j--;
+      }
+      
+      pos--;
+  }
+  
+  
+};

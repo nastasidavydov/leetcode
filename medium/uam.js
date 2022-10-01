e84d67f1b283f7d6784cada959cd56bf9d823d96
Nastasi Davydov <nastasidavydov@gmail.com>
2022-10-01 18:00:51 -0400

    find the users active minutes - hash table and set

---
+++ medium/uam.js
@@ -0,0 +1,30 @@
+/*You are given the logs for users' actions on LeetCode, and an integer k. The logs are represented by a 2D integer array logs where each logs[i] = [IDi, timei] indicates that the user with IDi performed an action at the minute timei.
+
+Multiple users can perform actions simultaneously, and a single user can perform multiple actions in the same minute.
+
+The user active minutes (UAM) for a given user is defined as the number of unique minutes in which the user performed an action on LeetCode. A minute can only be counted once, even if multiple actions occur during it.
+
+You are to calculate a 1-indexed array answer of size k such that, for each j (1 <= j <= k), answer[j] is the number of users whose UAM equals j.
+
+Return the array answer as described above. */
+
+var findingUsersActiveMinutes = function(logs, k) {
+  const ans = new Array(k).fill(0);
+  const uam = {};
+
+  for (const log of logs) {
+      let userID = log[0];
+      let actionMin = log[1];
+
+      if (uam[userID] === undefined) {
+          uam[userID] = new Set();
+      }
+      uam[userID].add(actionMin);
+  }
+
+  for (const uamList of Object.values(uam)) {
+      ans[uamList.size - 1]++;
+  }
+
+  return ans;
+};
